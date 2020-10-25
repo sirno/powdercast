e93654e0797ced629c45a642f223805450ccb1be
Nicolas Ochsner <ochsnern@student.ethz.ch>
2020-10-25 17:29:19 +0100

    Update constants.

--- const.py
+++ const.py
@@ -1,8 +1,13 @@
 """Constants for Powder Cast integration."""
+from datetime import timedelta
 
 DOMAIN = "powder_cast"
+
+SCAN_INTERVAL = timedelta(minutes=30)
 
 ATTR_DATE = "date"
 ATTR_SNOW_HEIGHT_BOT = "snow_height_bot"
 ATTR_SNOW_HEIGHT_MID = "snow_height_mid"
 ATTR_SNOW_HEIGHT_TOP = "snow_height_top"
+
+CONF_LOCATIONS = "locations"
